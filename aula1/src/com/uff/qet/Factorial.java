419738c7e39f94633b88218b9934d157d7e604c2
Leticia Vidal <leticiavna@id.uff.br>
2020-07-04 18:23:55 -0300

    add comments and iterative method

--- aula1/src/com/uff/qet/Factorial.java
+++ aula1/src/com/uff/qet/Factorial.java
@@ -1,11 +1,28 @@
 package com.uff.qet;
 
 public class Factorial {
-    public static long recursiveFactorial(long n) {
+    /**
+     * Calcula o fatorial de um número recursivamente
+     * @param n: int, fatorial a ser calculado
+     * @return fatorial
+     */
+    public static int recursiveFactorial(int n) {
         if(n < 0) throw new IllegalArgumentException("Number should be 0 or bigger.");
         /* if (n == 1 || n == 0), return 1 */
         if (n <= 1) return 1;
-        final long l = n * recursiveFactorial(n - 1);
-        return l;
+        return n * recursiveFactorial(n - 1);
+    }
+
+    /**
+     * Calcula o fatorial de um número iterativamente
+     * @param n: int, fatorial a ser calculado
+     * @return fatorial
+     */
+    public static int iterativeFactorial(int n) {
+        int finalValue = 1;
+        if (n < 0) throw new IllegalArgumentException("Number should be 0 or bigger.");
+        else if (n <= 1) return 1;
+        for (int i = 1; i <= n; i++) finalValue *= i;
+        return finalValue;
     }
 }
