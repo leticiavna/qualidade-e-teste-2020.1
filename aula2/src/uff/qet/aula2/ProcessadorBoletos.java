2ff7fc7689516a70501842752fa0611c12eaba21
Leticia Vidal <leticiavna@id.uff.br>
2020-07-11 17:38:24 -0300

    add implementação do processador de boletos

--- aula2/src/uff/qet/aula2/ProcessadorBoletos.java
+++ aula2/src/uff/qet/aula2/ProcessadorBoletos.java
@@ -5,5 +5,15 @@ import java.util.List;
 public abstract class ProcessadorBoletos {
 
     static void processar(List<Boleto> boletos, Fatura fatura) {
+        if (fatura == null) throw new IllegalArgumentException("Fatura enviada para o precessador não existe.");
+        if (boletos == null || boletos.isEmpty()) throw new IllegalArgumentException("Boletos enviados para o processador não existem.");
+        double totalPagamentos = 0.0;
+        for (Boleto boleto : boletos) {
+            Pagamento pagamento = new Pagamento(boleto.getValorPago(), "BOLETO");
+            fatura.setPagamentos(pagamento);
+            totalPagamentos += pagamento.getValorPago();
+        };
+
+        if (fatura.getValorTotal() <= totalPagamentos) fatura.setFaturaPaga();
     }
 }
